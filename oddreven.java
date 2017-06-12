0597812c5857475ebc913ec70cbb68c51b17c493
SowndharyaM <sowndharyaerd@gmail.com>
2017-06-12 14:27:05 +0530

    Create oddreven.java

--- oddreven.java
+++ oddreven.java
@@ -1,1 +1,22 @@
 import java.io.*;
+import java.util.*;
+class oddreven
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    if(n==0)
+    {
+      System.out.print("zero");
+    }
+    if(n%2==0)
+    {
+      System.out.print("Even");
+    }
+    else
+    {
+      System.out.print("Odd");
+    }
+  }
+} 
